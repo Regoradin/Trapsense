c79590eee089c012a3d2053878bb887f8cc5b59e
Oliver <oliver@theabates.com>
2019-01-21 15:11:49 -0800

    Spike Model
    Spike trap is now modeled, textured, animated, and imported.

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -88,6 +88,7 @@ public class Trap : MonoBehaviour {
 		Player player = other.GetComponent<Player>();
 		if(player != null)
 		{
+			Debug.Log(name + "Hitting player");
 			player.Damage(damage);
 			if (direction.ToString() != "None")
 			{
