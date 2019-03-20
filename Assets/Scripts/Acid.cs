922adec864773e41c9c680935854ec6a8df70871
Oliver <oliver@theabates.com>
2019-03-19 23:39:47 -0700

    Acid Environmental Effects
    Acid traps now color the floor green.

---
+++ Assets/Scripts/Acid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acid : MonoBehaviour
+{
+	public Color stain_color;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		other.GetComponentInChildren<Renderer>().material.color += stain_color;
+	}
+}
